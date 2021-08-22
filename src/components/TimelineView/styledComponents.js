b1040e8d6eb7168a7660f61f3951571638f1b859
Shaik Dadapeer <shaikdadapeer362@gmail.com>
2021-08-22 14:06:11 +0000

    commit message

---
+++ src/components/TimelineView/styledComponents.js
@@ -0,0 +1,11 @@
+// Style your elements here
+import styled from 'styled-components'
+
+export const TimeLineViewContainer = styled.div`
+  padding: 40px;
+`
+
+export const TimeLineViewHeading = styled.h1`
+  color: #000000;
+  text-align: center;
+`
